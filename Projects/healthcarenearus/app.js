5fcbc96c650f651d3a68a019315ce7f3fd907ed7
Sravanthi <sravanthidasari@hotmail.com>
2020-02-10 21:36:13 -0800

    validating ajax and input

--- Projects/healthcarenearus/app.js
+++ Projects/healthcarenearus/app.js
@@ -1,13 +1,49 @@
-// console.log("hello");
+const validateZipCode= () =>{
+    let zipCodeStr = $('#zipCodeInput').val();
+    let zipCodeNum = parseInt(zipCodeStr);
+    if (!zipCodeNum || zipCodeNum <= 9999 || zipCodeNum > 99999) {
+        alert('Invalid Zip Code. Try Again');
+    } else {
+        if(validateZipCode != true){
+            return ;
+        }
+    }
+};
 
-const getAddress= (e) => {
+const getAddress = (e) => {
     e.preventDefault();
+    validateZipCode();
+    if(validateZipCode != true){
+        return;
+    }
+    const zipCodeInput = $("#zipCodeInput").val();
     const $address = $('.address');
     $address.empty();
-    const $div = $("<div>").addClass("getAddress").text("18264 NE 111th ST, Redmond, WA-98052");
+    const $div = $("<div>").addClass("getAddress").text(zipCodeInput);
     $address.append($div);
 };
 
 $(() => {
+    // Attach a handler to toggle the description under about
+    $(".about").click(function() {
+        $("#p1").animate({
+            height: "toggle"
+        });
+    });
+    
+    // Attach a handler to fetch the entries based on zip
     $(".info button").on("click", getAddress);
+
+
 });
+
+// TODO: Move this to the right place
+$.ajax({
+    url:'https://data.medicare.gov/resource/xubh-q36u.json?zip_code=98101',
+    success: (data)=> {
+        console.log(data);
+    },
+    error:()=> {
+        console.log('Dude Your Stuff doesnt');
+    }
+})
